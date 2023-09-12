14d21ea67d214b3baad110ddaf15d99d4e96cfd0
shashippk <shashi.ppk@gmail.com>
2023-09-12 20:51:29 +0530

    made change for nextAuth i.e options for getting session for api

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
+
+import { nextOptions } from "./options";
 
 async function refreshToken(token) {
   const response = await fetch("https://accounts.spotify.com/api/token", {
@@ -32,57 +33,6 @@ async function refreshToken(token) {
   };
 }
 
-const handler = NextAuth({
-  providers: [
-    SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope:
-            "user-read-email playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-read-private",
-        },
-      },
-    }),
-  ],
-  secret: process.env.SECRET,
-  callbacks: {
-    async jwt({ token, account }) {
-      // if (account) {
-      //   // Save the access token and refresh token in the JWT on the initial login
-
-      //   return {
-      //     ...token,
-      //     access_token: account.access_token,
-      //     expires_at: date,
-      //     refresh_token: account.refresh_token,
-      //     spotify_id: account.providerAccountId,
-      //   };
-      // }
-      // if (Date() < token.expires_at) {
-      //   // If the access token has not expired yet, return it
-      //   return token;
-      // }
-      // return await refreshToken(token);
-      if (account) {
-        const date = new Date(account.expires_at * 1000);
-        token.access_token = account.access_token;
-        token.refresh_token = account.refresh_token;
-        token.spotify_id = account.providerAccountId;
-        //to milliseconds
-        token.expires_at = account.expires_at;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.user.accessToken = token.access_token;
-      session.user.refreshToken = token.refresh_token;
-      session.user.spotify_id = token.spotify_id;
-      session.user.expires_at = token.expires_at ?? "";
-      return session;
-    },
-  },
-});
+const handler = NextAuth(nextOptions);
 
 export { handler as GET, handler as POST };
--- app/api/findsong/route.js
+++ app/api/findsong/route.js
@@ -1,5 +1,8 @@
+import { getServerSession } from "next-auth/next";
 import { NextResponse } from "next/server";
+import { nextOptions } from "../auth/[...nextauth]/options";
 
 export async function GET() {
-  return NextResponse.json({ message: "success" });
+  const session = await getServerSession(nextOptions);
+  return NextResponse.json({ message: "success", session: session });
 }
