2f3ffe20406d58d21031f4c7a8347327c7cdfbfa
shashippk <shashi.ppk@gmail.com>
2023-10-31 22:05:50 +0530

    fix:build issue fix

--- context/SotifyContext.js
+++ context/SotifyContext.js
@@ -1,4 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+'use client'
 import { signOut, useSession } from "next-auth/react";
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
