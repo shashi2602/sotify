9a780d082cf3b71b3405cea48a23461cea0f59c1
shashi_ppk <43520737+shashi2602@users.noreply.github.com>
2024-06-24 20:16:18 +0530

    Update finderapi.js

--- utils/finderapi.js
+++ utils/finderapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://honestbeneficialbackend.shashippk.repl.co/findSong";
+const url = "https://58b0e37b-469e-4d15-84ea-67412b196ec1-00-3i88tyl6uf0dl.pike.replit.dev/findSong";
 
 export const finderPost = async (body) => {
   const { data } = await axios.post(url, body);
